5a5866d3fa082c599ae7d6a336cae261eaa46571
Matthias1590 <matthiasx95@gmail.com>
2021-12-10 10:33:59 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+from copy import deepcopy
 import pygame
 
 pygame.init()
 
 RESOLUTION = (80, 60)
+RESOLUTION_MULTIPLIER = 10
 
-display = pygame.display.set_mode(RESOLUTION)
+display = pygame.display.set_mode(
+    (RESOLUTION[0] * RESOLUTION_MULTIPLIER, RESOLUTION[1] * RESOLUTION_MULTIPLIER)
+)
 
 grid = []
-for y in range(0, RESOLUTION[1]):
+for y in range(RESOLUTION[1]):
     row = []
-    for x in range(0, RESOLUTION[0]):
+    for x in range(RESOLUTION[0]):
         row.append(0)
     grid.append(row)
 
+frame = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (
@@ -22,45 +27,60 @@ while True:
             exit()
 
     display.fill((0, 0, 0))
-    for y in range(0, RESOLUTION[1]):
-        for x in range(0, RESOLUTION[0]):
-            if grid[y][x] == 1:
-                pygame.draw.rect(display, (255, 255, 255), (x, y, 1, 1))
 
     if pygame.mouse.get_pressed()[0]:
         pos = pygame.mouse.get_pos()
 
-        grid[pos[1]][pos[0]] = 1
+        grid[pos[1] // RESOLUTION_MULTIPLIER][pos[0] // RESOLUTION_MULTIPLIER] = 1
+    elif pygame.mouse.get_pressed()[2]:
+        pos = pygame.mouse.get_pos()
 
-    new_grid = []
-    for y in range(0, RESOLUTION[1]):
-        row = []
-        for x in range(0, RESOLUTION[0]):
-            row.append(0)
-        new_grid.append(row)
+        grid[pos[1] // RESOLUTION_MULTIPLIER][pos[0] // RESOLUTION_MULTIPLIER] = 0
 
-    for y in range(0, RESOLUTION[1]):
-        for x in range(0, RESOLUTION[0]):
-            try:
-                neigbors = {
-                    "up": grid[y - 1][x],
-                    "down": grid[y + 1][x],
-                    "left": grid[y][x - 1],
-                    "right": grid[y][x + 1],
-                    "down_left": grid[y + 1][x - 1],
-                    "down_right": grid[y + 1][x + 1],
-                    "up_left": grid[y - 1][x - 1],
-                    "up_right": grid[y - 1][x + 1],
-                }
+    for y in range(RESOLUTION[1]):
+        for x in range(RESOLUTION[0]):
+            if grid[y][x] == 1:
+                pygame.draw.rect(
+                    display,
+                    (255, 255, 255),
+                    (
+                        x * RESOLUTION_MULTIPLIER,
+                        y * RESOLUTION_MULTIPLIER,
+                        RESOLUTION_MULTIPLIER,
+                        RESOLUTION_MULTIPLIER,
+                    ),
+                )
 
-                if neigbors["up"]:
-                    new_grid[y][x] = 1
-            except IndexError:
-                if y == 0:
-                    new_grid[y][x] = 1
-                # new_grid[y][x] = 0
+    new_grid = deepcopy(grid)
+    for y in range(RESOLUTION[1] - 1):
+        for x in range(RESOLUTION[0]):
+            current = grid[y][x]
 
-    grid = new_grid
+            if not current:
+                continue
+
+            neighbours = {
+                "up": grid[y - 1][x],
+                "down": grid[y + 1][x],
+                "left": grid[y][x - 1],
+                "right": grid[y][x + 1],
+                "up_left": grid[y - 1][x - 1],
+                "up_right": grid[y - 1][x + 1],
+                "down_left": grid[y + 1][x - 1],
+                "down_right": grid[y + 1][x + 1],
+            }
+
+            if not neighbours["down"]:
+                new_grid[y + 1][x] = 1
+                new_grid[y][x] = 0
+            elif not neighbours["down_left"]:
+                new_grid[y + 1][x - 1] = 1
+                new_grid[y][x] = 0
+            elif not neighbours["down_right"]:
+                new_grid[y + 1][x + 1] = 1
+                new_grid[y][x] = 0
+    grid = deepcopy(new_grid)
 
     pygame.display.update()
     pygame.time.Clock().tick(60)
+    frame += 1
